4090f5bc3cdcbc59d15f8289c00e129f28222d24
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-03-03 20:59:01 -0600

    navbar and json data

---
+++ src/javascripts/components/navBar.js
@@ -0,0 +1,41 @@
+const navBar = () => {
+  document.querySelector('#navigation').innerHTML = `
+    <nav class='navbar fixed-top navbar-expand-lg navbar-dark bg-dark mb-4'>
+        <a class='navbar-brand title' href='#'>Almost Amazon</a>
+        <button
+          class='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span class='navbar-toggler-icon'></span>
+        </button>
+        <div class='collapse navbar-collapse' id='navbarNav'>
+          <ul class='navbar-nav mr-auto'>
+            <li class='nav-item active'>
+              <a class='nav-link' href='#' id='all-boards'>
+                All Boards <span class='sr-only'>(current)</span>
+              </a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='#' id='favorites'>Favorites</a>
+            </li>
+            <li>
+            <input
+              class='form-control mr-sm-2 ml-3'
+              id='search'
+              placeholder='Search Collections'
+              aria-label='Search'
+            />
+            </li>
+          </ul>
+          <div id='logout-button'></div>
+        </div>
+      </nav>
+    `;
+};
+
+export default navBar;
